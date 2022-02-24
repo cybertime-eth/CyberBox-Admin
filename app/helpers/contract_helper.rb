120cc256228ca244ab2833d110b99580196a05c0
dennis <itdev1210@gmail.com>
2022-02-24 22:33:19 +0800

    add sleep time

--- app/helpers/contract_helper.rb
+++ app/helpers/contract_helper.rb
@@ -31,6 +31,7 @@ module ContractHelper
     end
 
     def fetchIPFSImage(contract_name)
+        sleep(1)
         logger.info("---------fetchIPFSImage started -------")
         contractInfo = ContractInfo.find_by(contract_info_id:contract_name)
         if contractInfo.present?
