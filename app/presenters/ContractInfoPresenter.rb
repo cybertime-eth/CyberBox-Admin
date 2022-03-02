41e12dc9dec519e446e0237f2513ff38ae20b038
dennis <itdev1210@gmail.com>
2022-03-03 02:09:35 +0800

    remove unused fields

--- app/presenters/ContractInfoPresenter.rb
+++ app/presenters/ContractInfoPresenter.rb
@@ -5,12 +5,7 @@ class ContractInfoPresenter < BasePresenter
             contract_info_id: contract_info_id,
             contract: contract,
             contract_id: contract_id,
-            price: price,
-            seller: seller,
-            owner: owner,
-            contract_address: contract_address,
             nftSymbol: nftSymbol,
-            market_status: market_status,
             name: name,
             rating_index: rating_index
         }
