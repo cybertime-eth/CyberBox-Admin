5e68f88daf09927064b7be6a531cba6559581de1
dennis <itdev1210@gmail.com>
2022-07-28 14:24:59 +0800

    change timeout

--- app/helpers/contract_helper.rb
+++ app/helpers/contract_helper.rb
@@ -359,7 +359,7 @@ module ContractHelper
                     puts "# makeThumbnail error"
                     @errorItem = ErrorImageItem.create(contract_info_id: contract_info_obj.id, contract_name: contract_info_obj.contract_info_id)
                     if nftSymbol == "nomstronaut"
-                        sleep(8)
+                        sleep(15)
                     end
                 end
             end
